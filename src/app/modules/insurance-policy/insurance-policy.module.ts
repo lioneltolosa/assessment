8c546a983dd98f8cbaba10e59bc6b5960d642738
lionelmiguel.tolosa <lionelmiguel.tolosa@altran.es>
2018-10-02 14:36:35 +0200

    Insurance-policy component

---
+++ src/app/modules/insurance-policy/insurance-policy.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PoliciesComponent } from './components/policies/policies.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [PoliciesComponent]
+})
+export class InsurancePolicyModule { }
