b052c379970616be49d9b14d912929dbe913a7a3
lionelmiguel.tolosa <lionelmiguel.tolosa@altran.es>
2018-10-03 15:15:49 +0200

    UPTATED Sykes, config and clean up

--- src/app/core/components/index.ts
+++ src/app/core/components/index.ts
@@ -1,2 +1,3 @@
 export { HeaderComponent } from './header/header.component';
 export { FooterComponent } from './footer/footer.component';
+export { CarouselComponent } from './carousel/carousel.component';
